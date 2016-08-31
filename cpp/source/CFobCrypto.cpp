16858d73e09aaa69a5ff4797e2d600f8dd088deb
Jaime Rios <online@jaimerios.com>
2016-08-30 23:10:03 -0400

    Changes suggested by library author; changed UTF8String typedef to std::string.

--- cpp/source/CFobCrypto.cpp
+++ cpp/source/CFobCrypto.cpp
@@ -16,44 +16,47 @@ namespace CFob
 {
     
 
-auto CreateDSAPubKeyFromPublicKeyPEM(const UTF8String publicKeyPEM) -> std::tuple<bool, ErrorMessage, DSA*>
+auto CreateDSAPubKeyFromPublicKeyPEM(const std::string publicKeyPEM) -> std::tuple<bool, ErrorMessage, DSA*>
 {
     if (publicKeyPEM.length()==0)
     {
-        return std::make_tuple(false, UTF8String{"Empty PEM string detected"}, nullptr);
+        return {false, std::string{"Empty PEM string detected"}, nullptr};
     }
     
     const auto completeKey = IsPublicKeyComplete(publicKeyPEM) ? publicKeyPEM : CompletePublicKeyPEM(publicKeyPEM);
+
+    ERR_load_crypto_strings();
     
     auto bio    = BIO_MEM_uptr{BIO_new_mem_buf((void *)completeKey.c_str(), -1), ::BIO_free};
     auto dsa    = DSA_new();
     auto result = PEM_read_bio_DSA_PUBKEY(bio.get(), &dsa, NULL, NULL);
-#if (0)
+#if (1)
     if (result != nullptr)
     {
-        return std::make_tuple(true, UTF8String{"Success"}, dsa);
+        return {true, std::string{"Success"}, dsa};
     }
     else
     {
 #if defined(DEBUG)
         ERR_print_errors_fp(stdout);
 #endif
-        return std::make_tuple(false, UTF8String{"Error detected"}, nullptr);
+        const auto message = std::string{ERR_error_string(ERR_get_error(), nullptr)};
+        return {false, message, nullptr};
     }
 #else
     (void)result;
-    return std::make_tuple(true, UTF8String{"Success"}, dsa);
+    return {true, std::string{"Success"}, dsa};
 #endif
 }
 
-auto IsPublicKeyComplete(const UTF8String publicKey) -> bool
+auto IsPublicKeyComplete(const std::string publicKey) -> bool
 {
-    auto found = publicKey.find(std::string{"-----BEGIN PUBLIC KEY-----"});
-    return found != std::string::npos;
+    const auto found = publicKey.find(std::string{"-----BEGIN DSA PUBLIC KEY-----"}) != std::string::npos;
+    return found;
 }
 
 
-auto CompletePublicKeyPEM(const UTF8String partialPEM) -> UTF8String
+auto CompletePublicKeyPEM(const std::string partialPEM) -> std::string
 {
     using namespace std::string_literals;
     
