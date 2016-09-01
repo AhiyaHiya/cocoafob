263ae3ba645d9a4dbb652347e42c04af883ed00d
Jaime Rios <online@jaimerios.com>
2016-08-31 21:32:00 -0400

    Added files for movable struct for managing DSA ptr.

---
+++ cpp/ctest/CFobDSAKeyPEM_ctest.cpp
@@ -0,0 +1,39 @@
+//
+//  CFobDSAKeyPEM_ctest.cpp
+//  cocoafob
+//
+//  Created by Jaime Rios on 2016-08-31.
+//  Copyright © 2016 Jaime O. Rios. All rights reserved.
+//
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "CFobDSAKeyPEM.hpp"
+#include "CFob_ctest_common.hpp"
+
+TEST_CASE("Bad key test", "")
+{
+    try
+    {
+        auto key = cocoafob::CFobDSAKeyPEM{cocoafob::KeyType::Private, std::string{""}};
+    }
+    catch (...)
+    {
+        CHECK(true);
+    }
+}
+
+TEST_CASE("Good key test", "")
+{
+    try
+    {
+        auto pubKey = GetPartialPublicKey();
+        auto key = cocoafob::CFobDSAKeyPEM{cocoafob::KeyType::Public, pubKey};
+    }
+    catch (...)
+    {
+        CHECK(true);
+    }
+}
+
+
---
+++ cpp/source/CFobDSAKeyPEM.cpp
@@ -0,0 +1,25 @@
+//
+//  CFobDSAKeyPEM.cpp
+//  cocoafob
+//
+//  Created by Jaime Rios on 2016-08-31.
+//  Copyright © 2016 Jaime O. Rios. All rights reserved.
+//
+
+#include "CFobDSAKeyPEM.hpp"
+
+namespace cocoafob
+{
+    
+    CFobDSAKeyPEM::CFobDSAKeyPEM(const KeyType keyType,
+                                 const std::string keyText)
+    : _keyType{keyType}
+    , _keyText{keyText}
+    {
+        if (_keyText.length()==0)
+        {
+         //   throw std::exception();
+        }
+    }
+    
+}
