e2da65439172dbe34176883353a60dfcfbd58f33
Jaime Rios <online@jaimerios.com>
2016-09-10 13:20:18 -0400

    Added mechanism to complete pem key on construction of DSAKeyPEM class.

--- cpp/source/CFobDSAKeyPEM.cpp
+++ cpp/source/CFobDSAKeyPEM.cpp
@@ -7,20 +7,42 @@
 //
 
 #include "CFobDSAKeyPEM.hpp"
+#include "CFobCrypto.hpp"
 
 namespace cocoafob
 {
+    auto TakeCareOfKey(const KeyType keyType, const std::string keyText)
+    {
+        if (keyText.length()==0)
+        {
+            return std::string{};
+        }
+        
+        auto isCompleteKey = IsKeyComplete(keyType, keyText);
+        
+        if(isCompleteKey)
+        {
+            return keyText;
+        }
+        else
+        {
+            auto completeKey = CompleteKeyPEM(keyType, keyText);
+            return completeKey;
+        }
+    }
     
     CFobDSAKeyPEM::CFobDSAKeyPEM(const KeyType keyType,
                                  const std::string keyText)
     : _keyType{keyType}
-    , _keyText{keyText}
+    , _keyText{TakeCareOfKey(keyType, keyText)}
     , _dsaKey{nullptr}
     {
         if (_keyText.length()==0)
         {
             throw std::exception();
         }
+        
+        
     }
     
     CFobDSAKeyPEM::operator DSA*() const
