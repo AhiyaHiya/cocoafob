d0bfca2023cfdaf1015a5832df56f1fba3a5c520
Jaime Rios <online@jaimerios.com>
2016-09-11 01:38:59 -0400

    Formatted file; added move constructor.

--- cpp/source/CFobDSAKeyPEM.cpp
+++ cpp/source/CFobDSAKeyPEM.cpp
@@ -9,78 +9,78 @@
 #include "CFobDSAKeyPEM.hpp"
 #include "CFobCrypto.hpp"
 
-#import <openssl/evp.h>
 #import <openssl/err.h>
+#import <openssl/evp.h>
 #import <openssl/pem.h>
 
 using namespace std::string_literals;
 
 namespace cocoafob
 {
+
+CFobDSAKeyPEM::CFobDSAKeyPEM(const KeyType keyType,
+                             const std::string keyText)
+    : _keyType{keyType}, _keyText{CheckKey(keyText)}, _dsaKey{SetUpDSAPtr()}
+{
+    ;
+}
+
+CFobDSAKeyPEM::CFobDSAKeyPEM(CFobDSAKeyPEM &&other)
+    : _keyType{other._keyType}, _keyText{other._keyText}, _dsaKey{other._dsaKey}
+{
     
-    
-    CFobDSAKeyPEM::CFobDSAKeyPEM(const KeyType keyType,
-                                 const std::string keyText)
-    : _keyType{keyType}
-    , _keyText{CheckKey(keyText)}
-    , _dsaKey{SetUpDSAPtr()}
-    {
-        ;
-    }
-    
-    CFobDSAKeyPEM::operator DSA*() const
-    {
-        return _dsaKey;
-    }
-    
-    /**
+}
+
+CFobDSAKeyPEM::operator DSA *() const
+{
+    return _dsaKey;
+}
+
+/**
      Precondition(s):
      - Non-empty string
      
      Postcondition(s):
      - Completes key if BEGIN/END missing from PEM string
      */
-    auto CFobDSAKeyPEM::CheckKey(const std::string keyText) -> std::string
+auto CFobDSAKeyPEM::CheckKey(const std::string keyText) -> std::string
+{
+    if (keyText.length() == 0)
     {
-        if (keyText.length()==0)
-        {
-            throw std::invalid_argument( "Non-empty DSA PEM key required"s );
-        }
-        
-        auto isCompleteKey = IsKeyComplete(_keyType, keyText);
-        
-        if(isCompleteKey)
-        {
-            return keyText;
-        }
-        else
-        {
-            auto completeKey = CompleteKeyPEM(_keyType, keyText);
-            return completeKey;
-        }
+        throw std::invalid_argument("Non-empty DSA PEM key required"s);
     }
-    
-    auto CFobDSAKeyPEM::SetUpDSAPtr() -> DSA*
+
+    auto isCompleteKey = IsKeyComplete(_keyType, keyText);
+
+    if (isCompleteKey)
     {
-        auto dsa    = DSA_new();
-        auto bio    = BIO_MEM_uptr{BIO_new_mem_buf((void *)_keyText.c_str(), -1), ::BIO_free};
-        
-        auto result =
-        _keyType == KeyType::Private ?
-        PEM_read_bio_DSAPrivateKey(bio.get(), &dsa, NULL, NULL) :
-        PEM_read_bio_DSA_PUBKEY(bio.get(), &dsa, NULL, NULL);
-        
-        if(result==nullptr)
-        {
-            DSA_free(dsa);
-            const auto message = (_keyType == KeyType::Private ? "Private"s : "Public"s) +
-            " key dsa failure"s;
-            throw std::runtime_error(message);
-        }
-        else
-        {
-            return dsa;
-        }
-        
+        return keyText;
     }
+    else
+    {
+        auto completeKey = CompleteKeyPEM(_keyType, keyText);
+        return completeKey;
+    }
+}
+
+auto CFobDSAKeyPEM::SetUpDSAPtr() -> DSA *
+{
+    auto dsa = DSA_new();
+    auto bio = BIO_MEM_uptr{BIO_new_mem_buf((void *)_keyText.c_str(), -1), ::BIO_free};
+
+    auto result =
+        _keyType == KeyType::Private ? PEM_read_bio_DSAPrivateKey(bio.get(), &dsa, NULL, NULL) : PEM_read_bio_DSA_PUBKEY(bio.get(), &dsa, NULL, NULL);
+
+    if (result == nullptr)
+    {
+        DSA_free(dsa);
+        const auto message = (_keyType == KeyType::Private ? "Private"s : "Public"s) +
+                             " key dsa failure"s;
+        throw std::runtime_error(message);
+    }
+    else
+    {
+        return dsa;
+    }
+}
 }
