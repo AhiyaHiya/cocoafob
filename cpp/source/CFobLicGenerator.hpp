b554d708509eb343c400e9f8540db2097b74aff0
Jaime Rios <online@jaimerios.com>
2016-09-11 00:15:30 -0400

    Removed factory function.

--- cpp/source/CFobLicGenerator.hpp
+++ cpp/source/CFobLicGenerator.hpp
@@ -21,55 +21,15 @@ class CFobLicGenerator
 {
 public:
     CFobLicGenerator(const std::string privateKey);
-//    CFobLicGenerator(DSA* privKey, const std::string privateKey);
-    
     auto SetPrivateKey() -> std::tuple<bool, ErrorMessage>;
-    
     auto GenerateRegCodeForName(const std::string name) -> std::tuple<bool, RegCode>;
     
 private:
-//    template <typename T>
-//    friend T CreateCFobLicGenerator(const std::string privateKey );
-    
-    
-    
     CFobLicGenerator() = delete;
     
-    //std::unique_ptr<DSA, decltype(&::DSA_free)> _dsaPrivKey;
     const std::string _privateKey;
 };
 
-#if (0)
-/*
- Factory function, which will check if the private key
- is valid before returning an instance to CFobLicGenerator.
- */
-template <typename T = std::shared_ptr<CFobLicGenerator> >
-T CreateCFobLicGenerator(const std::string privateKey )
-{
-    if (privateKey.length() == 0)
-        return T{};
-    
-    auto dsaKeyResult = CFob::CreateDSAPrivateKeyFromPrivateKeyPEM(privateKey);
-    
-    const auto success = std::get<0>(dsaKeyResult);
-    const auto reason  = std::get<1>(dsaKeyResult);
-    (void)reason; // for debugging purposes
-    
-    if (success)
-    {
-        auto privKey = std::get<2>(dsaKeyResult);
-        auto generator = T {new CFobLicGenerator(privKey, privateKey)};
-        
-        return generator;
-    }
-    else
-    {
-        return T{};
-    }
-}
-
-#endif
 
 }
 
