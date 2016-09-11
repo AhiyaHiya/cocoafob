484d93437d99a89ca0e5a571ecacc33f0313d7d1
Jaime Rios <online@jaimerios.com>
2016-09-11 00:14:20 -0400

    Minor edit; removed declarations for unused functions.

--- cpp/source/CFobCrypto.hpp
+++ cpp/source/CFobCrypto.hpp
@@ -15,16 +15,11 @@
 namespace cocoafob
 {
     auto IsKeyComplete(const KeyType keyType, const std::string keyText) -> bool;
-    
-    auto IsPublicKeyComplete(const std::string publicKey) -> bool;
-    
     auto CompleteKeyPEM(const KeyType keyType, const std::string partialPEM) -> std::string;
     
+    
+    auto IsPublicKeyComplete(const std::string publicKey) -> bool;
     auto CompletePublicKeyPEM(const std::string partialPEM) -> std::string;
-    
-    auto CreateDSAPubKeyFromPublicKeyPEM(const std::string publicKey) -> std::tuple<bool, ErrorMessage, DSA*>;
-    
-    auto CreateDSAPrivateKeyFromPrivateKeyPEM(const std::string privateKey) -> std::tuple<bool, ErrorMessage, DSA*>;
 }
 
 
