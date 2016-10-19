3544f6d239bbd50f7043a051da02fbd4be1b619a
Jaime O. Rios <online@jaimerios.com>
2016-10-18 22:57:51 -0400

    Fixed defects in build script.

--- cpp/build_project.sh
+++ cpp/build_project.sh
@@ -1,4 +1,26 @@
+#!/usr/bin/env bash
+
 set -e
 set -u
 
-xcodebuild -target cocoafob_ctest -configuration Debug -verbose CONFIGURATION_BUILD_DIR=xcbuild clean build
+CURRENT_PATH=$PWD
+
+function BuildUnitTests()
+{
+	cd mac
+	xcodebuild -project cocoafob.xcodeproj -target cocoafob_ctest -configuration Debug -verbose CONFIGURATION_BUILD_DIR=xcbuild clean build
+
+	xcodebuild -project cocoafob.xcodeproj -target cocoafob_ctest -configuration Release -verbose CONFIGURATION_BUILD_DIR=xcbuild clean build
+	cd $CURRENT_PATH
+}
+
+function BuildProject()
+{
+	cd mac
+	xcodebuild -project cocoafob.xcodeproj -target "static lib" -configuration Debug -verbose CONFIGURATION_BUILD_DIR=xcbuild clean build
+
+	xcodebuild -project cocoafob.xcodeproj -target "static lib" -configuration Release -verbose CONFIGURATION_BUILD_DIR=xcbuild clean build
+	cd $CURRENT_PATH
+}
+
+BuildProject
