e6c0412d1676f661c6134a60ce107021609c4bcf
Jaime O. Rios <xdev@openideasinc.com>
2017-06-10 00:48:01 -0400

    Modified download script to have separate win32 x64 build calls.

--- cpp/download_required_libs.bat
+++ cpp/download_required_libs.bat
@@ -1,43 +1,51 @@
 @ECHO OFF
-
+REM https://stackoverflow.com/questions/38572948/openssl-nmake-fatal-error-u1077-ias-return-code-0x1
 SET "LOCAL_CURRENT_PATH=%CD%"
 CALL:SetupOpenSSL
 EXIT/b
 
 :SetupOpenSSL
 	ECHO SetupOpenSSL
-	REM CALL :DownloadOpenSSL
-	CALL :BuildOpenSSL
+	REM CALL :DownloadAndBuild32Bit
+	CALL :DownloadAndBuild64Bit
+
+	CD %LOCAL_CURRENT_PATH%	
 	EXIT/b
 	
-:DownloadOpenSSL
-	IF EXIST %LOCAL_CURRENT_PATH%\components\openssl\win\tmp_src (
-		ECHO "components openssl win exist"
-	) ELSE (
-		ECHO "components DOESN'T exist"
-		MKDIR %LOCAL_CURRENT_PATH%\components\openssl\win\tmp_src
-	)
-	CD %LOCAL_CURRENT_PATH%\components\openssl\win\tmp_src
-	git.exe clone -b OpenSSL_1_0_2-stable https://github.com/openssl/openssl
-
-	EXIT/b 
 	
 REM Used information from http://developer.covenanteyes.com/building-openssl-for-visual-studio/
+REM https://stackoverflow.com/questions/34410711/how-to-build-openssl-in-vs2015-x86cpuid-asm-is-an-empty-file
 REM for below build settings
-:BuildOpenSSL
-	CD %LOCAL_CURRENT_PATH%\components\openssl\win\tmp_src\openssl
+:DownloadAndBuild32Bit
+	SET PATH=%PATH%;"C:\Program Files (x86)\NASM"
+	SET LOCAL_SRC_PATH=%LOCAL_CURRENT_PATH%\components\tmp_openssl_src_win32
+	MKDIR %LOCAL_SRC_PATH%
 	
-	perl Configure VC-WIN32 --prefix=%LOCAL_CURRENT_PATH%\components\openssl\win\tmp_src\openssl\build_win32
+	CD %LOCAL_SRC_PATH%
+	git.exe clone -b OpenSSL_1_0_2-stable https://github.com/openssl/openssl
+	
+	CD %LOCAL_SRC_PATH%\openssl
+	perl Configure VC-WIN32 --prefix=%LOCAL_SRC_PATH%\build_win32
 	CALL ms\do_ms
 	nmake -f ms\ntdll.mak
 	nmake -f ms\ntdll.mak test
 	nmake -f ms\ntdll.mak install
 	
-	CD %LOCAL_CURRENT_PATH%\components\openssl\win\tmp_src\openssl
-	perl Configure VC-WIN64A --prefix=%LOCAL_CURRENT_PATH%\components\openssl\win\tmp_src\openssl\build_x64
+	EXIT/b 
+	
+:DownloadAndBuild64Bit
+	SET PATH=%PATH%;"C:\Program Files\NASM"
+	SET LOCAL_SRC_PATH=%LOCAL_CURRENT_PATH%\components\tmp_openssl_src_x64
+	REM MKDIR %LOCAL_SRC_PATH%
+	REM CD %LOCAL_SRC_PATH%
+	REM git.exe clone -b OpenSSL_1_0_2-stable https://github.com/openssl/openssl
+	
+	CD %LOCAL_SRC_PATH%\openssl
+	perl Configure VC-WIN64A --prefix=%LOCAL_SRC_PATH%\build_x64
 	CALL ms\do_win64a
 	nmake -f ms\ntdll.mak
 	nmake -f ms\ntdll.mak test
 	nmake -f ms\ntdll.mak install 
 
 	EXIT/b 
+	
