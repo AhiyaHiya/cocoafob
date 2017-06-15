f723726818a26c4a856eb710ebffb355ebc7877c
Jaime O. Rios <xdev@openideasinc.com>
2017-06-14 22:28:35 -0400

    Code cleanup in batch file.

--- cpp/download_required_libs.bat
+++ cpp/download_required_libs.bat
@@ -6,8 +6,8 @@ EXIT/b
 
 :SetupOpenSSL
 	ECHO SetupOpenSSL
-	REM CALL :DownloadAndBuild32Bit
-	CALL :DownloadAndBuild64Bit
+	CALL :DownloadAndBuild32Bit
+	REM CALL :DownloadAndBuild64Bit
 
 	CD %LOCAL_CURRENT_PATH%	
 	EXIT/b
@@ -16,6 +16,8 @@ EXIT/b
 REM Used information from http://developer.covenanteyes.com/building-openssl-for-visual-studio/
 REM https://stackoverflow.com/questions/34410711/how-to-build-openssl-in-vs2015-x86cpuid-asm-is-an-empty-file
 REM for below build settings
+
+REM only works with "VS2015 x86 Native Tools Command Prompt"
 :DownloadAndBuild32Bit
 	SET PATH=%PATH%;"C:\Program Files (x86)\NASM"
 	SET LOCAL_SRC_PATH=%LOCAL_CURRENT_PATH%\components\tmp_openssl_src_win32
@@ -33,12 +35,13 @@ REM for below build settings
 	
 	EXIT/b 
 	
+REM only works with "VS2015 x64 Native Tools Command Prompt"
 :DownloadAndBuild64Bit
 	SET PATH=%PATH%;"C:\Program Files\NASM"
 	SET LOCAL_SRC_PATH=%LOCAL_CURRENT_PATH%\components\tmp_openssl_src_x64
-	REM MKDIR %LOCAL_SRC_PATH%
-	REM CD %LOCAL_SRC_PATH%
-	REM git.exe clone -b OpenSSL_1_0_2-stable https://github.com/openssl/openssl
+	MKDIR %LOCAL_SRC_PATH%
+	CD %LOCAL_SRC_PATH%
+	git.exe clone -b OpenSSL_1_0_2-stable https://github.com/openssl/openssl
 	
 	CD %LOCAL_SRC_PATH%\openssl
 	perl Configure VC-WIN64A --prefix=%LOCAL_SRC_PATH%\build_x64
