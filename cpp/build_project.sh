008d59057455770d7a3545b93f4f1288aab29b8c
Jaime O. Rios <xdev@openideasinc.com>
2017-09-07 15:44:21 -0400

    Added release function for local testing.

--- cpp/build_project.sh
+++ cpp/build_project.sh
@@ -6,24 +6,37 @@ set -u
 BuildUnitTests()
 {
 	cd mac
-	xcodebuild -project cocoafob.xcodeproj -target cocoafob_ctest -configuration Debug -verbose CONFIGURATION_BUILD_DIR=xcbuild clean build
+	xcodebuild -workspace cocoafob.xcworkspace -target cocoafob_ctest -configuration Debug -verbose CONFIGURATION_BUILD_DIR=xcbuild clean build
 
-	xcodebuild -project cocoafob.xcodeproj -target cocoafob_ctest -configuration Release -verbose CONFIGURATION_BUILD_DIR=xcbuild clean build
+	xcodebuild -workspace cocoafob.xcworkspace -target cocoafob_ctest -configuration Release -verbose CONFIGURATION_BUILD_DIR=xcbuild clean build
 	cd $CURRENT_PATH
 }
 
 BuildProject()
 {
 	cd mac
-	xcodebuild -project cocoafob.xcodeproj -target "static lib" -configuration Debug -verbose CONFIGURATION_BUILD_DIR=xcbuild clean build
+	xcodebuild -workspace cocoafob.xcworkspace -target "static lib" -configuration Debug -verbose CONFIGURATION_BUILD_DIR=xcbuild clean build
 
-	xcodebuild -project cocoafob.xcodeproj -target "static lib" -configuration Release -verbose CONFIGURATION_BUILD_DIR=xcbuild clean build
+	xcodebuild -workspace cocoafob.xcworkspace -target "static lib" -configuration Release -verbose CONFIGURATION_BUILD_DIR=xcbuild clean build
+	cd $CURRENT_PATH
+}
+
+BuildRelease()
+{
+	cd mac
+	local readonly XBUILD_DIR=xcbuild2
+	xcodebuild -workspace cocoafob.xcworkspace -scheme "install" -configuration Debug -verbose CONFIGURATION_BUILD_DIR=$XBUILD_DIR clean 
+	xcodebuild -workspace cocoafob.xcworkspace -scheme "install" -configuration Release -verbose CONFIGURATION_BUILD_DIR=$XBUILD_DIR clean 
+	
+	xcodebuild -workspace cocoafob.xcworkspace -scheme "install" -configuration Debug -verbose CONFIGURATION_BUILD_DIR=$XBUILD_DIR build
+	xcodebuild -workspace cocoafob.xcworkspace -scheme "install" -configuration Release -verbose CONFIGURATION_BUILD_DIR=$XBUILD_DIR build
 	cd $CURRENT_PATH
 }
 
 main()
 {
 	BuildProject
+	#BuildRelease
 }
 
 CURRENT_PATH=$PWD
